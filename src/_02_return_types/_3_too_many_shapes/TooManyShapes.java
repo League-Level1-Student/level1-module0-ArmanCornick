680e5a6850deb73ec22b6900de25b3d14b2dc334
league <league@imac-22.attlocal.net>
2023-11-25 12:57:31 -0800

    daw

--- src/_02_return_types/_3_too_many_shapes/TooManyShapes.java
+++ src/_02_return_types/_3_too_many_shapes/TooManyShapes.java
@@ -13,34 +13,41 @@ public class TooManyShapes {
 
 	public static void main(String[] args) {
 		//1. Ask the user how many sides they want their shape to be
-		
+		String sides1 = JOptionPane.showInputDialog("How many sides do you want on your shape?");
+		int sides = Integer.parseInt(sides1);
 		//2. Ask the user how many shapes they want
-		
+		String shapes1 = JOptionPane.showInputDialog("How many shapes do you want?");
+		int shapes = Integer.parseInt(shapes1);
 		//3. Call canMakeShape() and save what is returned into a variable
-		
+		boolean shape = canMakeShape(sides);
 		//4. If the shape CAN be drawn
-		
-			//5. Call and save what is returned from calculateTurnAngle()
-		
-			//6. Use drawPolygons() to draw your shape
-		
+		if(shape) {
+			int angle = calculateTurnAngle(sides);	
+			drawPolygons(sides, shapes, angle);
+		} else {
+			JOptionPane.showMessageDialog(null, notEnoughSides());
+		}
+		//5. Call and save what is returned from calculateTurnAngle()
+
+		//6. Use drawPolygons() to draw your shape
+
 		//7. If the shape CANNOT be drawn 
-			
-			//8. Call notEnoughSides() and print out what is returned 
-		
+
+		//8. Call notEnoughSides() and print out what is returned 
+
 	}
-	
+
 	static int calculateTurnAngle(int numSides) {
 		int angle = 360/numSides;
 		return angle;
 	}
-	
+
 	static void drawPolygons(int numSides, int numShapes, int degrees) {
 		rob.setSpeed(100);
 		rob.penDown();
 		int dist = 25;
 		Random rand = new Random();
-		
+
 		for(int i = 0; i < numShapes; i++) {
 			int x = rand.nextInt(600);
 			int y = rand.nextInt(600);
@@ -50,22 +57,22 @@ public class TooManyShapes {
 			rob.setY(y);
 			rob.setAngle(angle);
 			rob.setRandomPenColor();
-			
+
 			for(int j = 0; j < numSides; j++) {
 				rob.move(dist);
 				rob.turn(degrees);
 			}
-	    }
+		}
 		rob.hide();
 	}
-	
+
 	static boolean canMakeShape(int numSides) {
 		if(numSides >= 3) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	static String notEnoughSides() {
 		return "I'm sorry we cannot make a shape with that many sides\n"
 				+ "Please enter a number greater than 2";
--- src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -26,54 +26,66 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address” )
-
+		String pigeon = "https://live.staticflickr.com/7283/9531147088_7bfe6ed727_b.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component comp;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		comp = createImage(pigeon);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(comp);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String question = JOptionPane.showInputDialog("Do you like fat pigeons?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if(question.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "Correct");	
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong");	
+		}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		quizWindow.remove(comp);
 		// 10. find another image and create it (might take more than one line
 		// of code)
 
 		// 11. add the second image to the quiz window
-
+		String ferg = "https://i.redd.it/3wna7eaikmi51.jpg";
+		comp = createImage(ferg);
+		quizWindow.add(comp);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
-		// 14+ check answer, say if correct or incorrect, etc.
-
-	}
-
-	private Component createImage(String imageUrl) {
-		JLabel imageLabel = new JLabel();
-		URL url;
-		try {
-			url = new URL(imageUrl);
-			Icon icon = new ImageIcon(url);
-			imageLabel.setIcon(icon);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			JOptionPane.showMessageDialog(null, "I can't find your image!!");
+		String question2 = JOptionPane.showInputDialog("Do you like fergs?");
+		// 7. print "CORRECT" if the user gave the right answer
+		if(question2.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "Correct");	
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong");
 		}
-		
-		return imageLabel;
-	}
+			// 14+ check answer, say if correct or incorrect, etc.
 
-	/* OPTIONAL */
-	// *14. add scoring to your quiz
-	// *15. make something happen when mouse enters image
-	// (imageComponent.addMouseMotionListener())
-}
+		}
+
+		private Component createImage(String imageUrl) {
+			JLabel imageLabel = new JLabel();
+			URL url;
+			try {
+				url = new URL(imageUrl);
+				Icon icon = new ImageIcon(url);
+				imageLabel.setIcon(icon);
+			} catch (MalformedURLException ebkhb) {
+				// TODO Auto-generated catch block
+				JOptionPane.showMessageDialog(null, "I can't find your image!!");
+			}
+
+			return imageLabel;
+		}
+
+		/* OPTIONAL */
+		// *14. add scoring to your quiz
+		// *15. make something happen when mouse enters image
+		// (imageComponent.addMouseMotionListener())
+	}
