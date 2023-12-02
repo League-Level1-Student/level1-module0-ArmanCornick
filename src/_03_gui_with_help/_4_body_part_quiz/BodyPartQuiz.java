c526b7590014bd3400471683853bfff0302bea87
league <league@wts14-3.attlocal.net>
2023-12-02 13:00:02 -0800

    ads

--- src/_03_gui_with_help/_4_body_part_quiz/BodyPartQuiz.java
+++ src/_03_gui_with_help/_4_body_part_quiz/BodyPartQuiz.java
@@ -21,10 +21,10 @@ public class BodyPartQuiz {
 	// package,or if you prefer, get celebrity photos from the Internet,
 	// place them in the recipe package(body_part_quiz), and change the names below.
 
-	String firstImage = "src/_05_body_part_quiz/arnold.jpeg";
-	String secondImage = "src/_05_body_part_quiz/leonardo.jpeg";
-	String thirdImage = "src/_05_body_part_quiz/morgan.jpeg";
-	String fourthImage = "src/_05_body_part_quiz/jack.jpeg";
+	String firstImage = "src/_03_gui_with_help/_4_body_part_quiz/arnold.jpeg";
+	String secondImage = "src/_03_gui_with_help/_4_body_part_quiz/leonardo.jpeg";
+	String thirdImage = "src/_03_gui_with_help/_4_body_part_quiz/morgan.jpeg";
+	String fourthImage = "src/_03_gui_with_help/_4_body_part_quiz/jack.jpeg";
 
 	JFrame window = new JFrame();
 	JPanel panel = new JPanel();
@@ -42,7 +42,7 @@ public class BodyPartQuiz {
 
 		// 3. Change the size of the window so that you can only see part of the
 		// image.
-		window.setSize(500, 500);
+		window.setSize(500, 150);
 
 		showNextImage();
 
@@ -51,25 +51,54 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+		int score = 0;
 		// 2. Set the size of the window in the initializeGui() method 
-
+		
 		// 4. Ask the user who this person is and store their answer
 		String guess = JOptionPane.showInputDialog("who is this?");
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
-
+		if(guess.equalsIgnoreCase("arnold schwarzenegger")||guess.equalsIgnoreCase("arnold")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score += 1;
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong, it was Arnold Schwarznegger");		
+		}
 		// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is
-
+		
 		// 7. Use the showNextImage() method below to get the next image
+		
 		showNextImage();
+		String guess3 = JOptionPane.showInputDialog("who is this?");
+		if(guess3.equalsIgnoreCase("Leonardo Dicaprio")||guess3.equalsIgnoreCase("Leonardo")) {
+			JOptionPane.showMessageDialog(null, "Correct!");	
+			score += 1;
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong, it was Leonardo Dicaprio");		
+		}
+		showNextImage();
+		String guess112 = JOptionPane.showInputDialog("who is this?");
+		if(guess112.equalsIgnoreCase("Morgan Freeman")||guess112.equalsIgnoreCase("morgan")) {
+			JOptionPane.showMessageDialog(null, "Correct!");	
+			score += 1;
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong, it was Morgan Freeman");		
+		}
+		showNextImage();
+		String guess12 = JOptionPane.showInputDialog("who is this?");
+		if(guess12.equalsIgnoreCase("Jack black")||guess12.equalsIgnoreCase("jack")) {
+			JOptionPane.showMessageDialog(null, "Correct!");	
+			score += 1;
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong, it was Jack Black");		
+		}
 
 		// 8. .... repeat 4-7 for all your images.....
 
 		// 9. Show them their current score
-
+		JOptionPane.showMessageDialog(null, "Your score is " + score);
 	}
 
 	public void showNextImage() {
--- src/_03_gui_with_help/_5_duelling_buttons/DuellingButtons.java
+++ src/_03_gui_with_help/_5_duelling_buttons/DuellingButtons.java
@@ -21,29 +21,30 @@ public class DuellingButtons implements ActionListener {
 	Dimension BIG = new Dimension(400, 400);
 	Dimension SMALL = new Dimension(200, 200);
 
-	JFrame frame = new JFrame();
+	JFrame frame = new JFrame("Demanding Buttons");
 	JPanel panel = new JPanel();
 
 	public void run() {
 
 		// 1. Add the panel to the frame
-
+frame.add(panel);
 		// 2. Make the frame visible
-
+frame.setVisible(true);
 		// 3. Set the text of the leftButton to "Click me!"
-
+leftButton.setText("Click me");
 		// 4. Set the text of the rightButton to "Click me!"
+rightButton.setText("Click me");
 
 		// 5. Add an action listener to the leftButton
-
+leftButton.addActionListener(this);
 		// 6. Add an action listener to the rightButton
-
+rightButton.addActionListener(this);
 		// 7. Add the leftButton to the panel
-
+panel.add(leftButton);
 		// 8. Add the rightButton to the panel
-
+panel.add(rightButton);
 		// 9. Pack the frame
-
+		frame.pack();
 		// 10. Set the title of the frame to "Demanding Buttons"
 
 	}
@@ -51,7 +52,18 @@ public class DuellingButtons implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		JButton buttonPressed = (JButton) arg0.getSource();
-
+		if(buttonPressed==leftButton) {
+		rightButton.setText("No, click me!");	
+		leftButton.setText("Click me");
+		rightButton.setPreferredSize(BIG);
+		leftButton.setPreferredSize(SMALL);
+		}
+		if(buttonPressed==rightButton) {
+			leftButton.setText("No, click me!");	
+			rightButton.setText("Click me");
+			leftButton.setPreferredSize(BIG);
+			rightButton.setPreferredSize(SMALL);
+			}
 		/* If the buttonPressed was the leftButton.... */
 		// Set the text of the rightButton to "No, click Me!"
 		// Set the PREFERRED size of the rightButton to BIG
